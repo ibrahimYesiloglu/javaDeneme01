bc1d3498f6764f4e0e248e66f803ce47738661a1
ibrahimYesiloglu <cizgi.2d@gmail.com>
2023-01-26 22:49:20 +0100

    zweite aufgabe

--- src/Tag01/Student.java
+++ src/Tag01/Student.java
@@ -8,7 +8,16 @@ public class Student {
 
         System.out.println("Hallo 2" +
                 "Hallo 3" +
-                "Hallo4");
+                "Hallo4" +
+                "kdcl" +
+                "öldkfsldö" +
+                "sdlöfkölsdf" +
+                "lsdfölks" +
+                "ödäfl" +
+                "södlflösd" +
+                "sdkfksdlf" +
+                "#sdf" +
+                "sdf");
 
 
     }
